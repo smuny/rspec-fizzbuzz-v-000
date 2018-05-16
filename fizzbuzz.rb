40521d3121a5916849444c1a7cb4f6cd52b4180c
Sharon Mun <sharonymun@gmail.com>
2018-05-16 21:40:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  elsif number % 3 && 5 
+  elsif number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
-  elsif number !% 3 && 5
+  elsif number % 3 !== 0 || number % 5 !== 0
     return "nil"
   end
 end
